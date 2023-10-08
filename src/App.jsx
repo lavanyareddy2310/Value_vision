1747a37a70fa9e72158030579eabc8ab53438cfc
Lavanya Reddy <lavanyareddy2310@gmail.com>
2023-10-08 12:25:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Footer from './components/Footer'
+import Hero from './components/hero'
+import Mid from './components/Mid'
+import Newsletter from './components/Newsletter'
+
+function App() {
+  
+  return (
+   <>
+   <Hero/>
+    <Mid/>
+    <Newsletter/>
+    <Footer/>
+   </>
+  )
+}
+
+export default App
